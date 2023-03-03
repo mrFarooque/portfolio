bdad73b0324701f25254321df38b1b8c788be8e5
Farooque Ansari <120442803+mrFarooqueSmarthub@users.noreply.github.com>
2023-03-03 06:13:55 +0530

    portfolio found

--- report.js
+++ report.js
@@ -8,28 +8,28 @@ window.onscroll = function () {
   }
 };
 // BRIGNTNESS
-var light = document.querySelector("#lightbtn");
-var click = 0;
-light.addEventListener("click", lightFunction);
-function lightFunction() {
-  click++;
-  var body = document.querySelector("body");
-  if (click % 2 == 1) {
-    body.style.backgroundColor = "black";
-    document.getElementById("farooque").style.backgroundColor = "#2e2e2e";
-    document.getElementById("skills").style.backgroundColor = "#2e2e2e";
-    document.getElementById("footer").style.backgroundColor = "#2e2e2e";
-    var lightIcon = document.querySelector("#lightbtn");
-    lightIcon.innerHTML = '<i class="fa-solid fa-sun"></i>';
-  } else {
-    body.style.backgroundColor = "white";
-    document.getElementById("farooque").style.backgroundColor = "#007266";
-    document.getElementById("skills").style.backgroundColor = "#007266";
-    document.getElementById("footer").style.backgroundColor = "#007266";
-    var lightIcon = document.querySelector("#lightbtn");
-    lightIcon.innerHTML = '<i class="fa-solid fa-moon"></i>';
-  }
-}
+// var light = document.querySelector("#lightbtn");
+// var click = 0;
+// light.addEventListener("click", lightFunction);
+// function lightFunction() {
+//   click++;
+//   var body = document.querySelector("body");
+//   if (click % 2 == 1) {
+//     // body.style.backgroundColor = "black";
+//     document.getElementById("farooque").style.background = "#2e2e2e";
+//     document.getElementById("skills").style.backgroundColor = "#2e2e2e";
+//     document.getElementById("footer").style.backgroundColor = "#2e2e2e";
+//     var lightIcon = document.querySelector("#lightbtn");
+//     lightIcon.innerHTML = '<i class="fa-solid fa-sun"></i>';
+//   } else {
+//     // body.style.backgroundColor = "white";
+//     document.getElementById("farooque").style.background = "#007266";
+//     document.getElementById("skills").style.backgroundColor = "#007266";
+//     document.getElementById("footer").style.backgroundColor = "#007266";
+//     var lightIcon = document.querySelector("#lightbtn");
+//     lightIcon.innerHTML = '<i class="fa-solid fa-moon"></i>';
+//   }
+// }
 // NAVBAR MEDIA QUERY
 
 var togglebtn = document.querySelector(".nav-toggle");
